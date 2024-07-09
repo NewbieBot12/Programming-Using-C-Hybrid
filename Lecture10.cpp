432798a9f30643045d7951914785adaa5131fbb1
NewbieBot12 <163867484+NewbieBot12@users.noreply.github.com>
2024-07-09 15:28:33 +0530

    Add files via upload

---
+++ Lecture10.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    /*int i,j,k;
+    i=1,j=0,k=0;
+    for(i=1; ;i++){
+        cout<<i;
+    }
+    j=i++;
+    k=++k;
+    cout<<i<<endl;
+    cout<<j<<endl;
+    cout<<k<<endl;
+    cout<<i;*/
+    int i;
+    i=i+1;
+    for(i=1;i<=100;i++){
+     cout<<i<<" ";
+    }
+}
