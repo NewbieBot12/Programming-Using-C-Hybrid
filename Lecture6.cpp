2fb7c5008e16d8921716b6e57bb4206271bd07ac
NewbieBot12 <163867484+NewbieBot12@users.noreply.github.com>
2024-07-09 12:21:46 +0530

    Add files via upload

---
+++ Lecture6.cpp
@@ -0,0 +1,58 @@
+//If-Else Statement
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    
+
+    int age;
+    cout<<"enter your age : ";
+    cin>>age;
+
+    if(age>=18){
+        cout<<"you are adult and you can drive";
+    }
+    else if(age>=14 || age<18){
+        cout<<"you are a teenager";
+    }
+    else{
+        cout<<"you are not eligible to drive";
+    }
+    cout<<endl;
+    return 0;
+}
+
+
+//Swtich case statement
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    
+
+    int marks;
+    cin>>marks;
+    switch (marks)
+
+    {
+    case 90:
+    cout<<"your grade is O ";
+    break;
+    case 60:
+    cout<<"your grade is B+ ";
+    break;
+    case 70:
+    cout<<"your grade is A ";
+    break;
+    case 80:
+    cout<<"your grade is A+ ";
+    break;
+    default:
+    cout<<"you did not appeared for exams ";
+    break;
+    }
+
+    return 0;
+}
